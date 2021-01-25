87fa2a97995023233e49a536b5679b13f7715ab8
YuLaiZ <yulai_z@hotmailcom>
2021-01-25 15:47:34 +0800

    create project

---
+++ src/main/java/com/yulaiz/fund/note/service/FundNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yulaiz.fund.note.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundNoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundNoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yulaiz/fund/note/service/FundNoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulaiz.fund.note.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundNoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
